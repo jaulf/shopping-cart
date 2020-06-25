7509cc1404a2af2cb6472bd28e577e359ce4c01f
Howard Otuya <howardotuya17@gmail.com>
2020-06-25 23:03:06 +0100

    Updates

---
+++ precache-manifest.123d34493ba849d83c5c085acb0b0c68.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d152729cfa051c6a7d8336b9ba47ae",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "a5cda16dab6436906e83",
+    "url": "/shopping-cart/static/css/main.2974404c.chunk.css"
+  },
+  {
+    "revision": "045cf7b39150a5a7dd3f",
+    "url": "/shopping-cart/static/js/2.4539725a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart/static/js/2.4539725a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5cda16dab6436906e83",
+    "url": "/shopping-cart/static/js/main.03dafcd8.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  },
+  {
+    "revision": "412373dd1e979ca5bbc1157805895cb8",
+    "url": "/shopping-cart/static/media/1.412373dd.jpg"
+  },
+  {
+    "revision": "b4e3f2b82625fbbb12d069b7555b724d",
+    "url": "/shopping-cart/static/media/13.b4e3f2b8.jpg"
+  },
+  {
+    "revision": "53db3d7cf39130dae1732b6680ec8bfd",
+    "url": "/shopping-cart/static/media/15.53db3d7c.jpg"
+  },
+  {
+    "revision": "1c04a48631ce13a506e3cd426a2afb0b",
+    "url": "/shopping-cart/static/media/2.1c04a486.jpg"
+  },
+  {
+    "revision": "dd99b71de47679dc735ed997b2652980",
+    "url": "/shopping-cart/static/media/24.dd99b71d.jpg"
+  },
+  {
+    "revision": "44eca4515b99c9652ec9f24923703440",
+    "url": "/shopping-cart/static/media/33.44eca451.jpg"
+  }
+]);
